14571a28b5354e48a7043844118414c8441abf34
Ante <tolusic9@gmail.com>
2019-06-05 23:03:17 +0200

    init-v5

---
+++ route/user.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+
+router.get('/user', (req, res) => {
+  // Get all users
+})
+
+router.get('/user/:userId', (req, res) => {
+  const {userId} = req.query.param // eslint-disable-line
+  // Get by userId
+})
