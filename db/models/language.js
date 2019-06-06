60971dfa866ec74a3f2029ccf8c11eee78af592f
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2019-06-06 08:58:01 +0100

    first implimentation of serialize

---
+++ db/models/language.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Language = sequelize.define('Language', {
+    name: DataTypes.STRING
+  }, {});
+  Language.associate = function(models) {
+    // associations can be defined here
+  };
+  return Language;
+};
